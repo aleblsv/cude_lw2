d84f896b744dc44eb5b6fc97f0eeda202718fb0c
AlexBel <32871175+aleblsv@users.noreply.github.com>
2018-07-28 13:00:51 +0300

    Add min max module with index finding

---
+++ scr/max_min.cu
@@ -0,0 +1,223 @@
+//
+// Created by Alex on 15/6/2018.
+//
+// Example from: https://bitbucket.org/jsandham/algorithms_in_cuda
+//
+//
+
+#include "max_min.h"
+
+/* Private define ------------------------------------------------------------*/
+/* Private typedef -----------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+/* ---------------------------------------------------------------------------*/
+
+/**
+ *@brief  Find maximum in array,  GPU - kernel
+ *@param
+ *@retval None
+ */
+__global__ void find_maximum_kernel(float *array, float *max, int *mutex, unsigned int n)
+{
+    unsigned int index = threadIdx.x + blockIdx.x * blockDim.x;
+    unsigned int stride = gridDim.x * blockDim.x;
+    unsigned int offset = 0;
+
+    __shared__ float cache[256];
+
+
+    float temp = -MAX_MIN_INF;
+    while (index + offset < n)
+    {
+        temp = fmaxf(temp, array[index + offset]);
+
+        offset += stride;
+    }
+
+    cache[threadIdx.x] = temp;
+
+    __syncthreads();
+
+
+    // reduction
+    unsigned int i = blockDim.x / 2;
+    while (i != 0)
+    {
+        if (threadIdx.x < i)
+        {
+            cache[threadIdx.x] = fmaxf(cache[threadIdx.x], cache[threadIdx.x + i]);
+        }
+
+        __syncthreads();
+        i /= 2;
+    }
+
+    if (threadIdx.x == 0)
+    {
+        while (atomicCAS(mutex, 0, 1) != 0);  //lock
+        *max = fmaxf(*max, cache[0]);
+        atomicExch(mutex, 0);  //unlock
+    }
+}
+
+
+/**
+ *@brief  Find maximum in array,  GPU - kernel
+ *@param
+ *@retval None
+ */
+__global__ void find_maximum_index_kernel(float *array, float *max, int *maxIndex, int *mutex, unsigned int n)
+{
+    unsigned int index = threadIdx.x + blockIdx.x * blockDim.x;
+    unsigned int stride = gridDim.x * blockDim.x;
+    unsigned int offset = 0;
+    __shared__ float cache[256];
+    __shared__ int indexCache[256];
+
+    float temp = -1.0;
+    int tempIndex = 0;
+    while (index + offset < n)
+    {
+        if (temp < array[index + offset])
+        {
+            temp = array[index + offset];
+            tempIndex = index + offset;
+        }
+        offset += stride;
+    }
+    cache[threadIdx.x] = temp;
+    indexCache[threadIdx.x] = tempIndex;
+    __syncthreads();
+
+    // reduction
+    unsigned int i = blockDim.x / 2;
+    while (i != 0)
+    {
+        if (threadIdx.x < i)
+        {
+            if (cache[threadIdx.x] < cache[threadIdx.x + i])
+            {
+                cache[threadIdx.x] = cache[threadIdx.x + i];
+                indexCache[threadIdx.x] = indexCache[threadIdx.x + i];
+            }
+        }
+        __syncthreads();
+        i /= 2;
+    }
+
+    if (threadIdx.x == 0)
+    {
+        while (atomicCAS(mutex, 0, 1) != 0);  //lock
+        if (*max < cache[0])
+        {
+            *max = cache[0];
+            *maxIndex = indexCache[0];
+        }
+        atomicExch(mutex, 0);  //unlock
+    }
+}
+
+/**
+ *@brief  Find minimum in array,  GPU - kernel
+ *@param
+ *@retval None
+ */
+__global__ void find_minimum_kernel(float *array, float *min, int *mutex, unsigned int n)
+{
+    unsigned int index = threadIdx.x + blockIdx.x * blockDim.x;
+    unsigned int stride = gridDim.x * blockDim.x;
+    unsigned int offset = 0;
+
+    __shared__ float cache[256];
+
+
+    float temp = MAX_MIN_INF;
+    while (index + offset < n)
+    {
+        temp = fminf(temp, array[index + offset]);
+
+        offset += stride;
+    }
+
+    cache[threadIdx.x] = temp;
+
+    __syncthreads();
+
+
+    // reduction
+    unsigned int i = blockDim.x / 2;
+    while (i != 0)
+    {
+        if (threadIdx.x < i)
+        {
+            cache[threadIdx.x] = fminf(cache[threadIdx.x], cache[threadIdx.x + i]);
+        }
+
+        __syncthreads();
+        i /= 2;
+    }
+
+    if (threadIdx.x == 0)
+    {
+        while (atomicCAS(mutex, 0, 1) != 0);  //lock
+//        *min = fminf(*min, cache[0]);
+        *min = cache[0];
+        atomicExch(mutex, 0);  //unlock
+    }
+}
+
+/**
+ *@brief  Find maximum in array,  GPU - kernel
+ *@param
+ *@retval None
+ */
+__global__ void find_minimum_index_kernel(float *array, float *min, int *maxIndex, int *mutex, unsigned int n)
+{
+    unsigned int index = threadIdx.x + blockIdx.x * blockDim.x;
+    unsigned int stride = gridDim.x * blockDim.x;
+    unsigned int offset = 0;
+    __shared__ float cache[256];
+    __shared__ int indexCache[256];
+
+    float temp = MAX_MIN_INF;
+    int tempIndex = 0;
+    while (index + offset < n)
+    {
+        if (temp > array[index + offset])
+        {
+            temp = array[index + offset];
+            tempIndex = index + offset;
+        }
+        offset += stride;
+    }
+    cache[threadIdx.x] = temp;
+    indexCache[threadIdx.x] = tempIndex;
+    __syncthreads();
+
+    // reduction
+    unsigned int i = blockDim.x / 2;
+    while (i != 0)
+    {
+        if (threadIdx.x < i)
+        {
+            if (cache[threadIdx.x] > cache[threadIdx.x + i])
+            {
+                cache[threadIdx.x] = cache[threadIdx.x + i];
+                indexCache[threadIdx.x] = indexCache[threadIdx.x + i];
+            }
+        }
+        __syncthreads();
+        i /= 2;
+    }
+
+    if (threadIdx.x == 0)
+    {
+        while (atomicCAS(mutex, 0, 1) != 0);  //lock
+        *min = cache[0];
+        *maxIndex = indexCache[0];
+        atomicExch(mutex, 0);  //unlock
+    }
+}
